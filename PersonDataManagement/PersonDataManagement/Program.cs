0cf05236249493c5c2336677da9e8b42e13faed3
Piyushnmj <piyushnmj@yahoo.com>
2022-12-09 23:09:09 +0530

    Create Person Class, Create Variable for Person Class

--- PersonDataManagement/PersonDataManagement/Program.cs
+++ PersonDataManagement/PersonDataManagement/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Person Data Management");
+            List<Person> objListOfPersonsInCity = new List<Person>();
+            Repository objRep = new Repository();
+            objRep.AddRecords(objListOfPersonsInCity);
         }
     }
 }
